faa609ef334fbfab6ea1d9325ad031e3587d1b88
Edoardo Arturo Cancelli <edo.canc@gmail.com>
2025-04-28 21:25:43 +0200

    Fixed setup file entered version 0.0.1

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
-    version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    name='CommandCoin',
+    version='0.0.1',
+    description='Personal finance tracker',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Starlinetor',
+    author_email='eaccontent@gmail.com',
+    url='https://github.com/starlinetor/CommandCoin',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
