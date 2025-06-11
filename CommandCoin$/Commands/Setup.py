5baf85654f4613ca472cb5c3c30076a7b4df838c
Edoardo Arturo Cancelli <edo.canc@gmail.com>
2025-06-11 23:25:36 +0200

    changed settings setup function to work with the new database structure defined in the file 0.0.1_b

--- CommandCoin$/Commands/Setup.py
+++ CommandCoin$/Commands/Setup.py
@@ -1,10 +1,13 @@
+from ast import Str
 import os
 import sqlite3
 import time
+from tracemalloc import start
 import click
 from datetime import date
 from pathlib import Path
 from Commands.Utils.SQL import *
+import gc
 
 
 @click.group()
@@ -13,8 +16,10 @@ def setup() -> None:
     pass
 
 @setup.command()
+@click.confirmation_option(prompt='This will wipe your Config file and database, are you sure?')
 @click.option('-d','--directory', default=str(Path(__file__).parents[2] / "data"), help='Directory for database')
-def complete(directory:str) -> None:
+@click.option('-d','--start_date',type=click.DateTime(formats=["%Y-%d-%m"]), default=str(date.today()), help='Starting date (YYYY-MM-DD)')
+def complete(directory:str, start_date:str) -> None:
     """
     Complete setup for CommandCoin$\n
     It will wipe all old data
@@ -22,32 +27,49 @@ def complete(directory:str) -> None:
     #execute partial setups
     
     ctx = click.get_current_context()
-    ctx.invoke(set_db, directory=directory)
+    ctx.invoke(config_database, directory=directory, start_date=start_date)
     ctx.invoke(accounts)
     click.echo("Setup completed")
+    
 
 @setup.command(hidden=True)
-@click.confirmation_option(prompt='This will wipe your settings and database, are you sure?')
-@click.option('-d','--directory', default=str(Path(__file__).parents[2] / "data"), help='Directory for database')
-def set_db(directory:str) -> None:
+@click.confirmation_option(prompt='This will wipe your Accounts table in the database, are you sure?')
+def database() -> None:
     """
     [INTERNAL/DEBUG] use complete instead\n
-    Creates settings file and database, stores current date as starting date\n
+    Creates the Accounts table in the database\n
     It will wipe old data
-    """    
+    """
+    #get database
+    
+@setup.command(hidden=True)
+@click.confirmation_option(prompt='This will wipe your Config file and database, are you sure?')
+@click.option('-dir','--directory', default=str(Path(__file__).parents[2] / "data"), help='Directory for database')
+@click.option('-d','--start_date',type=click.DateTime(formats=["%Y-%d-%m"]), default=str(date.today()), help='Starting date (YYYY-MM-DD)')
+def config(directory:str, start_date:str) -> None:
+    """[INTERNAL/DEBUG] use complete instead\n
+    Creates Config database, stores current date as starting date\n
+    Wipes the old Config file and old database
+    """
     #wipe old files
     try:
         #remove old database
-        os.remove(get_setting("database_dir"))
-        #sleep needed to be sure that the settings file is released
-        time.sleep(0.1)
+        os.remove(get_data("database_dir"))
     except FileNotFoundError:
-        # File not found, not a problem
-        pass
+        #File not found, not a problem
+        click.echo("No previous database detected - initializing fresh installation", err=True) 
     except sqlite3.OperationalError:
-        click.echo("No previous database detected - initializing fresh installation", err=True)  
+        #Missing config database
+        click.echo("No previous database detected - initializing fresh installation", err=True) 
+    #sleep needed to be sure that the config file is released
+    time.sleep(0.1)
     try:
-        os.remove(settings_dir)
+        #Remove config database
+        os.remove(config_dir)
+    except PermissionError:
+        #exit the application if the file is open
+        click.echo("Config.db is open, close it before proceeding", err=True)  
+        exit()
     except FileNotFoundError:
         # File not found, not a problem
         pass  
@@ -56,36 +78,17 @@ def set_db(directory:str) -> None:
     database_dir : str = directory+"\\CommandCoin$.db"
     
     #open settings file and save data
-    conn : sqlite3.Connection = sqlite3.connect(settings_dir) 
+    conn : sqlite3.Connection = sqlite3.connect(config_dir) 
     cur : sqlite3.Cursor = conn.cursor()
-    cur.execute("CREATE TABLE settings(setting TEXT PRIMARY KEY, value TEXT)")
-    cur.execute(f"""INSERT OR REPLACE INTO settings VALUES
+    #create tables
+    cur.execute("CREATE TABLE settings(Setting TEXT PRIMARY KEY, Value TEXT)")
+    cur.execute("CREATE TABLE data(Name TEXT PRIMARY KEY, Value TEXT)")
+    cur.execute(f"""INSERT OR REPLACE INTO data VALUES
                 ('database_dir','{database_dir}'),
-                ('start_date','{str(date.today())}')
+                ('start_date','{start_date}')
                 """)
     conn.commit()
     conn.close()
     
     #create database
     sqlite3.connect(database_dir).close()
-    
-
-@setup.command(hidden=True)
-@click.confirmation_option(prompt='This will wipe your Accounts table in the database, are you sure?')
-def accounts() -> None:
-    """
-    [INTERNAL/DEBUG] use complete instead\n
-    Creates the Accounts table in the database\n
-    It will wipe old data
-    """
-    #get database
-    databse_dir : str = get_setting("database_dir")
-    conn : sqlite3.Connection = sqlite3.connect(databse_dir)
-    cur : sqlite3.Cursor = conn.cursor()
-    
-    #wipe old table and create a new one
-    cur.execute("DROP TABLE IF EXISTS Accounts")
-    cur.execute("CREATE TABLE Accounts(id TEXT PRIMARY KEY, name TEXT)")
-    conn.commit()
-    #close connection
-    conn.close()
--- CommandCoin$/Commands/Utils/SQL.py
+++ CommandCoin$/Commands/Utils/SQL.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import sqlite3
 
-settings_dir : str = Path(__file__).parents[3] / "data\\settings.db"
+config_dir : str = Path(__file__).parents[3] / "data\\Config.db"
 
 def get_setting(key:str) -> str:
     """Returns the value of a given setting
@@ -13,16 +13,55 @@ def get_setting(key:str) -> str:
         setting (str): value of the setting
         
     """
+    try:
+        with sqlite3.connect(config_dir)  as conn:  
+            #get cursor
+            cur : sqlite3.Cursor = conn.cursor()
+            #selects the column setting from the table settings
+            cur.execute(f"SELECT Value FROM settings WHERE Setting='{key}'")
+            #get value
+            setting = cur.fetchone()[0]
+            conn.close()
+            #return value
+            return setting
+    except sqlite3.Error as e:
+        #Something went wrong, just close the connection and return nothing
+        print(e)
+        print(f"{key} was not found, returned an empty string")
+        conn.close()
+        return ""
+    finally:
+        conn.close()
+    
+
+def get_data(key:str) -> str:
+    """Returns the value of a data entry in the config file
+
+    Args:
+        key (str): name of the data
+
+    Returns:
+        data (str): value of the data
+        
+    """
     #get connection with database
-    conn : sqlite3.Connection = sqlite3.connect(settings_dir) 
-    #get cursor
-    cur : sqlite3.Cursor = conn.cursor()
-    #selects the column setting from the table settings
-    cur.execute(f"SELECT value FROM settings WHERE setting='{key}'")
-    #get value
-    setting = cur.fetchone()[0]
-    #close connection
-    conn.close()
-    #return value
-    return setting
+    try:
+        with sqlite3.connect(config_dir)  as conn:  
+            #get cursor
+            cur : sqlite3.Cursor = conn.cursor()
+            #selects the column setting from the table settings
+            cur.execute(f"SELECT Value FROM data WHERE Name='{key}'")
+            #get value
+            data = cur.fetchone()[0]
+            conn.close()
+            #return value
+            return data
+    except sqlite3.Error as e:
+        #Something went wrong, just close the connection and return nothing
+        print(e)
+        print(f"{key} was not found, returned an empty string")
+        conn.close()
+        return ""
+    finally:
+        conn.close()
 
