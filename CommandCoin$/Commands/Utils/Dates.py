ebc993c6aef5742440feffb710d1e7af4aeb03b5
Edoardo Arturo Cancelli <edo.canc@gmail.com>
2025-08-07 15:04:50 +0200

    removed useless formating for the hours

--- CommandCoin$/Commands/Utils/Dates.py
+++ CommandCoin$/Commands/Utils/Dates.py
@@ -3,6 +3,6 @@ import time
 import Commands.Utils.SQL as SQL
 
 def get_date_id(date:datetime.datetime) -> int:
-    start_date : datetime.date = datetime.datetime.strptime(SQL.get_data("start_date"), "%Y-%m-%d %H:%M:%S").date()
+    start_date : datetime.date = datetime.datetime.strptime(SQL.get_data("start_date"), "%Y-%m-%d").date()
     delta : datetime.timedelta = date-start_date 
     return delta.days
